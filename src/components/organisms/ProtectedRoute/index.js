22956438aa6cef762f3158f88ed26300466e4368
kranthikata <kranthikumarkata464@gmail.com>
2024-11-09 21:01:59 +0530

    Update ProtectedRoute

--- src/components/organisms/ProtectedRoute/index.js
+++ src/components/organisms/ProtectedRoute/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
 const ProtectedRoute = (props) => {
-  const { accessToken } = localStorage.getItem("userInfo");
-  if (accessToken === undefined) {
+  const loggedInUser = JSON.parse(localStorage.getItem("userInfo"));
+  if (loggedInUser === null) {
     return <Redirect to="/" />;
   }
   return <Route {...props} />;
