f6f379b768734f6964c1b5c06890ef9dac332869
kranthikata <kranthikumarkata464@gmail.com>
2024-11-10 15:47:56 +0530

    Added no contacts view

--- src/components/organisms/ContactsSection/index.js
+++ src/components/organisms/ContactsSection/index.js
@@ -6,11 +6,44 @@ import ModelContext from "../../../context/ModelContext";
 import Icon from "../../atoms/Icon";
 import { TailSpin } from "react-loader-spinner";
 import AddGroupChatModel from "../../organisms/AddGroupChatModel";
+import Paragraph from "../../atoms/Paragraph";
 
 const ContactsSection = () => {
   const { contacts, isLoading, activeItem, updateActiveItem } =
     useContext(ContactsContext); // Using the context of contacts
   const { setShowCreateModel, showCreateModel } = useContext(ModelContext);
+  const renderLoading = () => (
+    <div className="w-full h-screen flex justify-center items-center">
+      <Icon icon={TailSpin} width={35} height={35} />
+    </div>
+  );
+
+  const renderContacts = () => (
+    <ContactsList
+      contacts={contacts}
+      activeItem={activeItem}
+      toggleActiveItem={updateActiveItem}
+    />
+  );
+
+  const renderNoContacts = () => (
+    <div className="w-full h-screen flex justify-center items-center">
+      <Paragraph className="text-black text-lg">
+        No Contacts Available
+      </Paragraph>
+    </div>
+  );
+
+  const renderCurrentView = (contacts, isLoading) => {
+    switch (true) {
+      case isLoading:
+        return renderLoading();
+      case contacts.length === 0:
+        return renderNoContacts();
+      default:
+        return renderContacts();
+    }
+  };
   return (
     <>
       <div
@@ -21,17 +54,7 @@ const ContactsSection = () => {
         <ContactsHeader
           toggleModel={() => setShowCreateModel((prevState) => !prevState)}
         />
-        {isLoading ? (
-          <div className="w-full h-screen flex justify-center items-center">
-            <Icon icon={TailSpin} width={35} height={35} />
-          </div>
-        ) : (
-          <ContactsList
-            contacts={contacts}
-            activeItem={activeItem}
-            toggleActiveItem={updateActiveItem}
-          />
-        )}
+        {renderCurrentView(contacts, isLoading)}
       </div>
       {showCreateModel && <AddGroupChatModel />}
     </>
